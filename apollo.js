3fd7b01d574c901f55dce9a307c7d529f8e43a26
jjcasco99 <juanjocasco28@gmail.com>
2026-05-23 19:41:32 +0200

    First steps mocking data

---
+++ apollo.js
@@ -0,0 +1,11 @@
+import { HttpLink } from "@apollo/client";
+import { InMemoryCache, ApolloClient } from "@apollo/client";
+
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://rickandmortyapi.com/graphql",
+  }),
+  cache: new InMemoryCache(),
+});
+
+export default client
